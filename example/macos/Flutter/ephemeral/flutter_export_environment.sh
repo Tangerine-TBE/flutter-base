610181639052034f902dc959691fbc1cbb621424
Tennysion <947624811@qq.com>
2026-05-13 14:38:20 +0800

    1.upgrade

---
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\fvm_cache\versions\3.44.0-0.2.pre"
+export "FLUTTER_APPLICATION_PATH=D:\Projects\base\base\example"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\Projects\base\base\example\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
